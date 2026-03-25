21c8ad181cff4b1e64d84f1478093c5efb200b4e
andreh <andre.hovda@gmail.com>
2026-03-25 12:10:26 +0100

    Change background color of dir in prompt to have better contrast

--- zsh/.p10k_custom.zsh
+++ zsh/.p10k_custom.zsh
@@ -1,1 +1,4 @@
 # Custom settings that override p10k.zsh goes here since p10k.zsh is generated with p10k configure
+
+# Background color of directory in prompt
+typeset -g POWERLEVEL9K_DIR_BACKGROUND=60
