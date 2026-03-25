c992f72120de8e30ca4bde5173ea1784b8816b87
andreh <andre.hovda@gmail.com>
2026-03-25 18:09:52 +0100

    Adjust window decoration due to issue with the bottom of terminal reaching below screen

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -10,8 +10,8 @@ config.color_scheme = 'Atelierdune (light) (terminal.sexy)'
 config.font = wezterm.font('JetBrains Mono', { weight = 'Medium' })
 config.font_size = 14
 config.window_background_opacity = 0.95
---config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "TITLE"
+config.hide_tab_bar_if_only_one_tab = true
+--config.window_decorations = "TITLE"
 
 -- Use zsh by default
 config.default_prog = { "/usr/bin/zsh", "-l" }
