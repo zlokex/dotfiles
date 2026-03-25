727c388a71061f995f9daeae77b9e1092a199c0a
andreh <andre.hovda@gmail.com>
2026-03-25 20:27:08 +0100

    Add Kitty and fix prompt rendering issue on resize with p10k (Kitty only)

--- zsh/.p10k_custom.zsh
+++ zsh/.p10k_custom.zsh
@@ -2,3 +2,10 @@
 
 # Background color of directory in prompt
 typeset -g POWERLEVEL9K_DIR_BACKGROUND=60
+
+# Fix bug where powerlevel10k prompt duplicates and renders in wrong areas upon resizing window.
+# The issue issue arises when a terminal reflows Zsh prompt upon resizing, but is more prone to
+# appear with powerlevel10k prompts than a normal prompt.
+# The following line fixes the issue for kitty, but the bug remains for alacritty and wezterm.
+# See: https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#mitigation
+typeset -g POWERLEVEL9K_TERM_SHELL_INTEGRATION=true
