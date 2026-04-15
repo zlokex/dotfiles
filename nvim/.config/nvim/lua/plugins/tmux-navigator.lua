e4df34a7d0acc00ee638550b7d1f4cdf9e98e23f
andreh <andre.hovda@gmail.com>
2026-04-15 20:56:03 +0200

    Disable tmux navigator keybindings due to onflicts
    C-hjkl conflicts with navigation between splits

--- nvim/.config/nvim/lua/plugins/tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,5 +1,8 @@
 return {
   'christoomey/vim-tmux-navigator',
+  init = function()
+    vim.g.tmux_navigator_no_mappings = 1
+  end,
   cmd = {
     'TmuxNavigateLeft',
     'TmuxNavigateDown',
@@ -9,10 +12,10 @@ return {
     'TmuxNavigatorProcessList',
   },
   keys = {
-    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-    --{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" }, -- In use by toggleterm
+    -- { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+    -- { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+    -- { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+    -- { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+    -- { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" }, -- In use by toggleterm
   },
 }
